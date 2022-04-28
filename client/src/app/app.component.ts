47e6cca3564b0c414c7c41cb54d11535a5178fc6
unknown <ahmedhamdyyehia@gmail.com>
2022-04-28 07:14:21 +0200

    end of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+
 // import { error } from 'console';
 
 @Component({
@@ -12,18 +11,18 @@ import { IProduct } from './models/product';
 export class AppComponent implements OnInit {
   title = 'Ecommerce';
 
-  Products:IProduct[];
-  constructor(private http:HttpClient)
+  // Products:IProduct[];
+  constructor()
   {
 
   }
   ngOnInit(): void {
-    this.http.get('https://localhost:7225/api/products?PageSize=50').subscribe((Response:IPagination)=> {
-      this.Products=Response.data;
-      console.log(Response);
-    },error=>{
-      console.log(error);
-    });
+    // this.http.get('https://localhost:7225/api/products?PageSize=50').subscribe((Response:IPagination)=> {
+    //   this.Products=Response.data;
+    //   console.log(Response);
+    // },error=>{
+    //   console.log(error);
+    // });
 
   }
 }
